e44ac39179f473330a34045f8f796458a207ffee
ephra <ephra@example.com>
2026-05-17 22:16:12 +0300

    Add cpm-mbita to CORS

--- backend/src/server.js
+++ backend/src/server.js
@@ -21,6 +21,7 @@ const app = express();
 app.use(helmet());
 app.use(cors({
   origin: [
+    'https://cpm-mbita.vercel.app',
     'https://church-website-app1.vercel.app',
     'https://church-website-app1-git-main-1235-hues-projects.vercel.app',
     'http://localhost:5173'
